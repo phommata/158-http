d536211dd609056efb8c0385276b8291612437c9
Andrew Phommathep <13destinies@gmail.com>
2016-12-01 23:38:33 -0800

    Implement HttpService & Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from "./http.service";
+import { Response } from "@angular/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+ constructor(private httpService: HttpService){
+
+ }
+
+  ngOnInit(){
+    this.httpService.getData()
+      .subscribe(
+        (data: Response) => console.log(data)
+      );
+  }
 }
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Http } from "@angular/http";
 
 @Injectable()
 export class HttpService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  getData(){
+    return this.http.get('https://angular2-course-61bbe.firebaseio.com/title.json');
+  }
 }
