47a106465a0c978a729ca508ce25272bb9a55211
Andrew Phommathep <13destinies@gmail.com>
2016-12-04 13:55:04 -0800

    162 Sending a POST Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,4 +17,8 @@ export class AppComponent implements OnInit {
         (data: any) => console.log(data)
       );
   }
+
+  onSubmit(username: string, email: string){
+    this.httpService.sendData({username: username, email: email}).subscribe(data => console.log(data));
+  }
 }
